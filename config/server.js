fcfbf81d3913fd85f98636de5a99dcd26d18fdf2
hauptdigital <marc@haupt.digital>
2021-09-14 23:25:58 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1322a2c08bd29827a49a746e76999ccd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0aba4244-084f-4db0-876f-b700c891b054'
+};
